25222b516e038d59185fbf1268ca8572ba5785b1
maks <maks.rus.in@mail.ru>
2022-01-28 02:08:36 +0300

    Lesson 4

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя', max_length=100)
-    description = models.TextField(verbose_name='описание', blank=True)
+    name = models.CharField(verbose_name="имя", max_length=100)
+    description = models.TextField(verbose_name="описание", blank=True)
 
     def __str__(self):
         return self.name
@@ -12,17 +12,14 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='имя', max_length=100)
+    name = models.CharField(verbose_name="имя", max_length=100)
     price = models.DecimalField(
-        verbose_name='цена', max_digits=7, decimal_places=2, default=0)
-    color = models.PositiveBigIntegerField(
-        verbose_name='цвет', default=0x000000)
-    description = description = models.TextField(
-        verbose_name='описание', blank=True)
-    image = models.ImageField(verbose_name='картинка', blank=True, upload_to='products')
-    quantity = models.PositiveBigIntegerField(
-        verbose_name='количество', default=0)
-    
+        verbose_name="цена", max_digits=7, decimal_places=2, default=0
+    )
+    color = models.PositiveBigIntegerField(verbose_name="цвет", default=0x000000)
+    description = description = models.TextField(verbose_name="описание", blank=True)
+    image = models.ImageField(verbose_name="картинка", blank=True, upload_to="products")
+    quantity = models.PositiveBigIntegerField(verbose_name="количество", default=0)
 
     def __str__(self):
         return self.name
